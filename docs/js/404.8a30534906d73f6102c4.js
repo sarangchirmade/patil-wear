ffb5134b10d04b071429e3c571e8745365db96d7
Sarang Chirmade <sarang.chirmade@deskera.org>
2022-06-04 14:51:39 +0530

    changes

---
+++ docs/js/404.8a30534906d73f6102c4.js
@@ -0,0 +1,30 @@
+"use strict";
+(self["webpackChunkecommerce_react"] = self["webpackChunkecommerce_react"] || []).push([[404],{
+
+/***/ 35404:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(67294);
+ // Just add this feature if you want :P
+
+var UserWishListTab = function UserWishListTab() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "loader",
+    style: {
+      minHeight: '80vh'
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", null, "My Wish List"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("strong", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+    className: "text-subtle"
+  }, "You don't have a wish list")));
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (UserWishListTab);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=404.8a30534906d73f6102c4.js.map
