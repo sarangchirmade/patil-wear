9a3950eb521be8f7b3d58bae41c32fac04675bf6
Sarang Chirmade <sarang.chirmade@deskera.org>
2022-06-04 14:32:32 +0530

    chnages

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,45 @@
+import * as Route from 'constants/routes';
+import logo from 'images/logo.jpg';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Footer = () => {
+  const { pathname } = useLocation();
+
+  const visibleOnlyPath = [
+    Route.HOME,
+    Route.SHOP
+  ];
+
+  return !visibleOnlyPath.includes(pathname) ? null : (
+    <footer className="footer">
+      <div className="footer-col-1">
+        {/* <strong>
+          <span>
+            Developed by
+            {' '}
+            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+          </span>
+        </strong> */}
+      </div>
+      <div className="footer-col-2">
+        {/* <img alt="Footer logo" className="footer-logo" src={logo} /> */}
+        <h2>Patil Mens Wear</h2>
+        <h5>
+          &copy;&nbsp;
+          {new Date().getFullYear()}
+        </h5>
+      </div>
+      {/* <div className="footer-col-3">
+        <strong>
+          <span>
+            Fork this project &nbsp;
+            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+          </span>
+        </strong>
+      </div> */}
+    </footer>
+  );
+};
+
+export default Footer;
